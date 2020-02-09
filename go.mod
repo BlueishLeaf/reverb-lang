808d57f31e889feb0944578e2ad79d6d08bfc53d
Killian Kelly <killiank360@gmail.com>
2020-02-09 03:06:57 +0000

    Fixes #1 Created basic cli from scratch

--- go.mod
+++ go.mod
@@ -1,8 +1,3 @@
 module github.com/BlueishLeaf/reverb-lang
 
 go 1.13
-
-require (
-	github.com/hajimehoshi/oto v0.5.4
-	github.com/urfave/cli/v2 v2.1.1
-)
---
+++ main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"log"
+	"os"
+	"strings"
+
+	"github.com/BlueishLeaf/reverb-lang/evaluator"
+	"github.com/BlueishLeaf/reverb-lang/lexer"
+	"github.com/BlueishLeaf/reverb-lang/object"
+	"github.com/BlueishLeaf/reverb-lang/parser"
+	"github.com/BlueishLeaf/reverb-lang/repl"
+)
+
+func main() {
+	if len(os.Args) == 2 {
+		file, err := os.Open(os.Args[1])
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer file.Close()
+		scanner := bufio.NewScanner(file)
+		scanner.Split(bufio.ScanLines)
+		var lines []string
+		for scanner.Scan() {
+			lines = append(lines, scanner.Text())
+		}
+		code := strings.Join(lines, "\n")
+		l := lexer.New(code)
+		p := parser.New(l)
+		program := p.ParseProgram()
+		environment := object.NewEnvironment()
+		evaluated := evaluator.Eval(program, environment)
+		if evaluated != nil {
+			_, _ = io.WriteString(os.Stdout, evaluated.Inspect())
+			_, _ = io.WriteString(os.Stdout, "\n")
+		}
+	} else {
+		fmt.Printf("Welcome to the Reverb REPL. Press Ctrl + D to exit.\n")
+		repl.Start(os.Stdin, os.Stdout)
+	}
+}
